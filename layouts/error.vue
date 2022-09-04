dced53a957eaad8af13d9c2ba324a33793576aaa
Sergey Kharitonov <kharitonov.si@gmail.com>
2022-09-04 20:34:43 +0300

    add h1 * 2 to error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,8 @@
 <template>
     <section>
         <h1>Error 404</h1>
+        <h1>Error 403</h1>
+        <h1>Error 402</h1>
         <nuxt-link to="/">Home</nuxt-link>
     </section>
 </template>
