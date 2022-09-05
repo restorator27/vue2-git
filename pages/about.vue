afc6a23eb681f6796896dfa3b1220f25802e18c7
Sergey Kharitonov <kharitonov.si@gmail.com>
2022-09-05 23:27:40 +0300

    add new paragraph to about

--- pages/about.vue
+++ pages/about.vue
@@ -3,6 +3,7 @@
         <h1>About page</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem aperiam sunt recusandae nobis? Labore tenetur debitis dolore? Beatae, pariatur? Corporis esse enim sint vero cumque ipsa asperiores voluptas similique mollitia!</p>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem aperiam sunt recusandae nobis? Labore tenetur debitis dolore? Beatae, pariatur? Corporis esse enim sint vero cumque ipsa asperiores voluptas similique mollitia!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem aperiam sunt recusandae nobis? Labore tenetur debitis dolore? Beatae, pariatur? Corporis esse enim sint vero cumque ipsa asperiores voluptas similique mollitia!</p>
     </section>
 </template>
 
